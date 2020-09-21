0fdf13a1ec25263949455db6d727edc523874e33
AmirSPC <amirspc76@gmail.com>
2020-09-22 00:52:41 +0330

    Create A VueJS Client && ExpressJS Server :-)

---
+++ client/src/views/Register.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Register</h2>
+        <input type="email" v-model="email" name="email" placeholder="Enter Your Email..">
+        <br>
+        <input type="password" v-model="password" name="password" placeholder="Enter Your PassWord..">
+        <br>
+        <button type="submit">Register</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            email:'',
+            password:''
+        }
+    }
+}
+</script>
---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+/////////////////////////
+const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const morgan=require('morgan');
+const app=express();
+const port=process.env.PORT || 8181;
+/////////////////////////
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+/////////////////////////
+app.get('/register',(req,res)=>{
+    res.send({
+        message:`Hello ${req.body.email}. Your Email Was Registered. Have Fun :-)`
+    });
+});
+/////////////////////////
+app.listen(port,()=>console.log(`\n\tApp Running On PORT ${port}\n`));
